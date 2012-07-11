646fe266f64c7e7d893f33babf1b01b82052fc66
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-07-11 11:25:27 +0200

    Custom member SQL exists purely for reference purposes now.

--- legacyauth/sql/member.sql
+++ legacyauth/sql/member.sql
@@ -1,1 +1,2 @@
-CREATE INDEX "email" ON "auth_user" ("email");
+# Hard-coded workaround to create an index on the email field in Django's user # model. This line exists purely for reference purposes.
+# CREATE INDEX "email" ON "auth_user" ("email");
